1c3a5b7514da01e58c4e285287a639722d4fca76
lllluciaL <161424449+lllluciaL@users.noreply.github.com>
2024-03-06 21:45:43 +0800

    Delete barchart directory

--- barchart/backend/AvgYearMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.apda.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ruoyi.apda.domain.AvgYear;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AvgYearMapper extends BaseMapper<AvgYear> {
-}
--- barchart/backend/IAvgYearService.java
+++
@@ -1,7 +0,0 @@
-package com.ruoyi.apda.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.ruoyi.apda.domain.AvgYear;
-
-public interface IAvgYearService extends IService<AvgYear> {
-}

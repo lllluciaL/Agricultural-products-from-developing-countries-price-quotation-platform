713ea221d71865c28e6db86dc72d91c02c737586
lllluciaL <161424449+lllluciaL@users.noreply.github.com>
2024-03-06 21:46:00 +0800

    Delete boxplot directory

--- boxplot/backend/FoodLaborMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.apda.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ruoyi.apda.domain.FoodLabor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FoodLaborMapper extends BaseMapper<FoodLabor> {
-}
--- boxplot/backend/IFoodLaborService.java
+++
@@ -1,7 +0,0 @@
-package com.ruoyi.apda.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.ruoyi.apda.domain.FoodLabor;
-
-public interface IFoodLaborService extends IService<FoodLabor> {
-}
